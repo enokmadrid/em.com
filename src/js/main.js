bcd0c27d7762c44cbb76b9af26ec794f36ba96c3
Enok Madrid <enokslaboratory@gmail.com>
2016-12-13 17:14:48 -0800

    mobile menu on bottom, hero fullscreen, scroll arrow fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
 //jQuery
 $( document ).ready(function() {
+    // Defining a function to set size for #hero
+    function fullscreen(){
+        $('.hero').css({
+            width: $(window).width(),
+            height: $(window).height()
+        });
+    }
+
+    fullscreen();
+
+    // Run the function in case of window resize
+    $(window).resize(function() {
+        fullscreen();
+    });
 
 });
 
