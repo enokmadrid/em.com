5ae7fb078bcc8e0640b8adf352a041c652131dfb
Enok Madrid <enokslaboratory@gmail.com>
2018-06-21 16:59:58 -0700

    major update, clean navigation, css updates

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 $( document ).ready(function() {
     // Defining a function to set size for #hero
     function fullscreen(){
-        $('.hero').css({
+        $('.fullscreen').css({
             width: $(window).width(),
             height: $(window).height()
         });
